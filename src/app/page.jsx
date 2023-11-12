db7e16a874de875bf049d977b7eb09e4b8514809
rminoro <rminoro@outlook.com>
2023-11-12 02:57:41 -0300

    estilização home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Bem-Vindo a PORTO HELP escolha a opção que desejar na parte superior do site</h1>
+      <h1 id="h1Home">Bem-Vindo a PORTO HELP escolha a opção que desejar na parte superior do site</h1>
   
     </div>
   )
